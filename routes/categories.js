f8d559cbbbfcbd01c3963ad0b3970cd065e0b0f3
daerxi <bli3@unb.ca>
2021-04-28 00:31:25 -0300

    get user by id

--- routes/categories.js
+++ routes/categories.js
@@ -2,19 +2,12 @@ const express = require('express');
 const {Category} = require("../sync");
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    try {
-        Category.findAll().then(async categories => {
-            res.status(200).send(categories);
-        })
-    } catch (error) {
+router.get('/', async function(req, res, next) {
+    Category.findAll().then(categories => {
+        res.status(200).json(categories);
+    }).catch(error => {
         res.status(400).json({error});
-    }
-});
-
-router.post('/:type', function(req, res, next) {
-
+    })
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const {Business} = require("../sync");
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BetterShop' });
+  try {
+    Business.findAll().then(async businesses => {
+      res.status(200).json(businesses);
+    });
+  } catch (error) {
+    res.status(400).json({error});
+  }
+});
+
+router.get('/:type', function(req, res, next) {
+  try {
+    Business.findAll({
+      where: {
+        category: type
+      }
+    }).then(async businesses => {
+      res.status(200).json(businesses);
+    });
+  } catch (error) {
+    res.status(400).json({error});
+  }
 });
 
 module.exports = router;
