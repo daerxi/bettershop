82efee0793260db57dc04c3d7ba9570e59fd8fae
daerxi <bli3@unb.ca>
2021-05-03 13:57:44 -0300

    integrate with donate api

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,20 @@ module.exports = router;
 
 router.post('/donate', async function (req, res, next) {
     try {
+        const amount = req.body.amount
         const stripe = require('stripe')(process.env.STRIPE_SECRETKEY);
         stripe.customers.create({
             name: req.body.name,
             email: req.body.email,
             source: req.body.stripeToken
         }).then(customer => stripe.charges.create({
-            amount: req.body.amount.toFixed(2) * 100,
+            amount: amount * 100,
             currency: req.body.currency,
             customer: customer.id,
-            description: 'Thank you for your Donation.'
-        })).then(() => {
+            receipt_email: req.body.email
+        })).then(response => {
             // send email
+            res.status(201).json(response)
         });
     } catch (error) {
         res.status(400).json({error: error.toString()});
