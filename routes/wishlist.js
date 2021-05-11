2e68e0d1e8603d1a53639ba8c384e502bccfa0ff
daerxi <bli3@unb.ca>
2021-05-11 13:04:56 -0300

    add wishlist

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {Op} = require("sequelize");
 const {getBusinessById} = require("../controllers/businesses");
 const {decodeJWT, checkToken} = require("../controllers/auth");
 const {WishList} = require("../sync");
@@ -35,5 +36,31 @@ router.post('/', checkToken, async function (req, res, next) {
             res.status(400).json({error: "Update wishlist failed."});
     }).catch(e => res.status(400).json({error: "Update wishlist failed.", reason: e.toString()}));
 });
+
+router.get('/:businessId', checkToken, async function (req, res, next) {
+    try {
+        const userId = decodeJWT(req.header.token).sub;
+        const businessId = parseInt(req.params.businessId);
+        WishList.findAll({
+            where: {
+                [Op.and]: [
+                    {
+                        userId
+                    },
+                    {
+                        businessId
+                    }
+                ]
+            }
+        }).then(async wishlist => {
+            if (wishlist) res.status(200).json({success: true});
+            else res.status(200).json({success: false});
+        }).catch(e => res.status(400).json({error: e.toString()}));
+    } catch (e) {
+        res.status(400).json({error: e.toString()});
+    }
+
+});
+
 
 module.exports = router;
